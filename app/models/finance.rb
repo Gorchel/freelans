7c158095378519e663ea724fae244116d880650e
Gorchel <kasatka567@gmail.com>
2015-05-11 17:46:12 +0300

    Validation

--- app/models/finance.rb
+++ app/models/finance.rb
@@ -1,3 +1,5 @@
 class Finance < ActiveRecord::Base
 	belongs_to :user
+	validates :user_id, presence: true
+	validates :cash, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
 	has_many :finances
+	validates :nick, presence: true
+	validates :mail, presence: true
+	validates :phone, presence: true
+	validates :pass, presence: true
+	validates :skype, presence: true
+	validates :rate, presence: true
 end
